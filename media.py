c60a27d4a4bab1b1c10de52c172955e93ef7abf4
BaniNaveen <baninkumar@gmail.com>
2017-08-23 13:10:05 +0530

    Update media.py

--- media.py
+++ media.py
@@ -5,7 +5,8 @@ class Movie():
     # The BluePrint of Movie Information
     def __init__(bani, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        # Initialize Movie Title, Movie Storyline, Poster Images of Movies and Youtube Trailer    # NOQA
+        # Initialize Movie Title, Movie Storyline, 
+        # Poster Images of Movies and Youtube Trailer
         bani.title = movie_title
         bani.storyline = movie_storyline
         bani.poster_image_url = poster_image
